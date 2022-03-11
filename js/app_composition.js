9bf79443c42da1fc9f7fffe86e03e38d028b4992
Angel Aguirre <angelaguirrel@hotmail.com>
2022-03-11 16:03:06 -0600

    List rendering

--- js/app_composition.js
+++ js/app_composition.js
@@ -21,14 +21,72 @@ const app = createApp({
             offer: true,
         });
 
-        const cartOpen = ref(false);
+        const cartOpen = ref(true);
 
         const activeImage = ref(0);
+
+        const cart = [
+            {
+                name: 'Camara 300',
+                price: 7_500,
+                stock: 0,
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                images: [
+                    {
+                        image: './img/camara.jpg',
+                        thumbnail: './img/camara-thumb.jpg',
+                    },
+                    {
+                        image: './img/camara-2.jpg',
+                        thumbnail: './img/camara-2-thumb.jpg',
+                    },
+                ],
+                new: true,
+                offer: true,
+            },
+            {
+                name: 'Camara 200',
+                price: 5_500,
+                stock: 0,
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                images: [
+                    {
+                        image: './img/camara.jpg',
+                        thumbnail: './img/camara-thumb.jpg',
+                    },
+                    {
+                        image: './img/camara-2.jpg',
+                        thumbnail: './img/camara-2-thumb.jpg',
+                    },
+                ],
+                new: true,
+                offer: true,
+            },
+            {
+                name: 'Camara 100',
+                price: 5_000,
+                stock: 0,
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                images: [
+                    {
+                        image: './img/camara.jpg',
+                        thumbnail: './img/camara-thumb.jpg',
+                    },
+                    {
+                        image: './img/camara-2.jpg',
+                        thumbnail: './img/camara-2-thumb.jpg',
+                    },
+                ],
+                new: true,
+                offer: true,
+            },
+        ]
 
         return {
             product,
             activeImage,
             cartOpen,
+            cart,
         }
     },
     mounted() {
--- js/app_options.js
+++ js/app_options.js
@@ -21,8 +21,64 @@ const app = createApp({
                 new: true,
                 offer: true,
             },
-            cartOpen: false,
+            cartOpen: true,
             activeImage: 0,
+            cart: [
+                {
+                    name: 'Camara 300',
+                    price: 7_500,
+                    stock: 0,
+                    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                    images: [
+                        {
+                            image: './img/camara.jpg',
+                            thumbnail: './img/camara-thumb.jpg',
+                        },
+                        {
+                            image: './img/camara-2.jpg',
+                            thumbnail: './img/camara-2-thumb.jpg',
+                        },
+                    ],
+                    new: true,
+                    offer: true,
+                },
+                {
+                    name: 'Camara 200',
+                    price: 5_500,
+                    stock: 0,
+                    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                    images: [
+                        {
+                            image: './img/camara.jpg',
+                            thumbnail: './img/camara-thumb.jpg',
+                        },
+                        {
+                            image: './img/camara-2.jpg',
+                            thumbnail: './img/camara-2-thumb.jpg',
+                        },
+                    ],
+                    new: true,
+                    offer: true,
+                },
+                {
+                    name: 'Camara 100',
+                    price: 5_000,
+                    stock: 0,
+                    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                    images: [
+                        {
+                            image: './img/camara.jpg',
+                            thumbnail: './img/camara-thumb.jpg',
+                        },
+                        {
+                            image: './img/camara-2.jpg',
+                            thumbnail: './img/camara-2-thumb.jpg',
+                        },
+                    ],
+                    new: true,
+                    offer: true,
+                },
+            ]
         }
     }
 });
