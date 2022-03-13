304bbe9cf48bef2a7f91f7ecc89cf0381004cc14
Angel Aguirre <angelaguirrel@hotmail.com>
2022-03-12 22:12:23 -0600

    Se cambió a reactive state y se agregaron toRefs

--- js/app_composition.js
+++ js/app_composition.js
@@ -1,60 +1,59 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, reactive, toRefs } = Vue;
 
 const app = createApp({
     setup() {
-        const product = ref({
-            name: 'Camara',
-            price: 5_500,
-            stock: 8,
-            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
-            images: [
-                {
-                    image: './img/camara.jpg',
-                    thumbnail: './img/camara-thumb.jpg',
-                },
-                {
-                    image: './img/camara-2.jpg',
-                    thumbnail: './img/camara-2-thumb.jpg',
-                },
-            ],
-            new: false,
-            offer: true,
-            quantity: 1,
+        const productState = reactive({
+            product: {
+                name: 'Camara',
+                price: 5_500,
+                stock: 8,
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
+                images: [
+                    {
+                        image: './img/camara.jpg',
+                        thumbnail: './img/camara-thumb.jpg',
+                    },
+                    {
+                        image: './img/camara-2.jpg',
+                        thumbnail: './img/camara-2-thumb.jpg',
+                    },
+                ],
+                new: false,
+                offer: true,
+                quantity: 1,
+            },
+            activeImage: 0,
         });
 
-        const cartOpen = ref(false);
-
-        const activeImage = ref(0);
-
-        const cart = ref([]);
+        const cartState = reactive({
+            cartOpen: false,
+            cart: [],
+        });
 
         const discountCodes = ref(['PLATZI2022', 'PLATZI20']);
 
         function applyDiscount(event) {
             const discountCodeIndex = discountCodes.value.indexOf(event.target.value);
             if ( discountCodeIndex >= 0 ) {
-                product.value.price *= 50 / 100;
+                productState.product.price *= 50 / 100;
                 discountCodes.value.splice(discountCodeIndex, 1);
             }
         }
 
         function addToCart() {
-            const prodIndex = cart.value.findIndex(prod => prod.name === product.value.name);
+            const prodIndex = cartState.cart.findIndex(prod => prod.name === productState.product.name);
 
             if ( prodIndex >= 0 ) {
-                cart.value[prodIndex].quantity += 1;
+                cartState.cart[prodIndex].quantity += 1;
             } else {   
-                cart.value.push(product.value);
+                cartState.cart.push(productState.product);
             }
-            product.value.stock -= 1;
+            productState.product.stock -= 1;
         }
 
         return {
-            product,
-            activeImage,
-            cartOpen,
-            cart,
-            discountCodes,
+            ...toRefs(productState),
+            ...toRefs(cartState),
             applyDiscount,
             addToCart,
         }
