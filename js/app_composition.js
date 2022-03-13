a0feeef30d3dc8838234c0e32b4078e9677e8187
Angel Aguirre <angelaguirrel@hotmail.com>
2022-03-12 19:57:30 -0600

    Agregando Métodos en composition api

--- js/app_composition.js
+++ js/app_composition.js
@@ -19,74 +19,44 @@ const app = createApp({
             ],
             new: false,
             offer: true,
+            quantity: 1,
         });
 
         const cartOpen = ref(false);
 
         const activeImage = ref(0);
 
-        const cart = [
-            {
-                name: 'Camara 300',
-                price: 7_500,
-                stock: 0,
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
-                images: [
-                    {
-                        image: './img/camara.jpg',
-                        thumbnail: './img/camara-thumb.jpg',
-                    },
-                    {
-                        image: './img/camara-2.jpg',
-                        thumbnail: './img/camara-2-thumb.jpg',
-                    },
-                ],
-                new: true,
-                offer: true,
-            },
-            {
-                name: 'Camara 200',
-                price: 5_500,
-                stock: 0,
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
-                images: [
-                    {
-                        image: './img/camara.jpg',
-                        thumbnail: './img/camara-thumb.jpg',
-                    },
-                    {
-                        image: './img/camara-2.jpg',
-                        thumbnail: './img/camara-2-thumb.jpg',
-                    },
-                ],
-                new: true,
-                offer: true,
-            },
-            {
-                name: 'Camara 100',
-                price: 5_000,
-                stock: 0,
-                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem consequatur iure, quas voluptatem quasi ea reiciendis quam expedita placeat mollitia culpa labore nobis ab, et praesentium laudantium consectetur adipisci consequuntur!',
-                images: [
-                    {
-                        image: './img/camara.jpg',
-                        thumbnail: './img/camara-thumb.jpg',
-                    },
-                    {
-                        image: './img/camara-2.jpg',
-                        thumbnail: './img/camara-2-thumb.jpg',
-                    },
-                ],
-                new: true,
-                offer: true,
-            },
-        ]
+        const cart = ref([]);
+
+        const discountCodes = ref(['PLATZI2022', 'PLATZI20']);
+
+        function applyDiscount(event) {
+            const discountCodeIndex = discountCodes.value.indexOf(event.target.value);
+            if ( discountCodeIndex >= 0 ) {
+                product.value.price *= 50 / 100;
+                discountCodes.value.splice(discountCodeIndex, 1);
+            }
+        }
+
+        function addToCart() {
+            const prodIndex = cart.value.findIndex(prod => prod.name === product.value.name);
+
+            if ( prodIndex >= 0 ) {
+                cart.value[prodIndex].quantity += 1;
+            } else {   
+                cart.value.push(product.value);
+            }
+            product.value.stock -= 1;
+        }
 
         return {
             product,
             activeImage,
             cartOpen,
             cart,
+            discountCodes,
+            applyDiscount,
+            addToCart,
         }
     },
     mounted() {
